82bae89d47e3983f42480014af84452e555515b1
Valentin Vasiliu <vali15tnt51v@gmail.com>
2024-06-02 19:59:08 +0200

    Try again after changing the increasing instance types in AWS EB

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          What up, homies !!!!
+          What up, homies !!!
         </p>
         <a
           className="App-link"
